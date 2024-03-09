448540bef59cc7eba92de1db2db4e2f4812199c5
Stephen Foster <stephen@thoughtstem.com>
2024-03-09 14:39:33 -0800

    revising toward mvp

--- asl-trainer/src/App.js
+++ asl-trainer/src/App.js
@@ -11,7 +11,10 @@ import {HashRouter as Router, Routes, Route, Link} from "react-router-dom"
 import { useStopwatch } from 'react-timer-hook';
 import useWindowSize from 'react-use/lib/useWindowSize'
 import Confetti from 'react-confetti'
-import { Typography } from '@mui/material';
+import { Stack, Typography } from '@mui/material';
+import Slide from '@mui/material/Slide';
+import Fade from '@mui/material/Fade';
+
 
 
 import NavBar from "./NavBar" 
@@ -33,11 +36,16 @@ SC - Sign choice
 let aslCards = [
   {
     clip: Clips.s3_host + '/clips/error-game/hello-your-name-what-incorrect.mp4',
+    clipGloss: "...",
     correctClip: Clips.s3_host + '/clips/error-game/hello-your-name-what-correct.mp4',
+    correctClipGloss: "...",
     type: "POA",
     english: "Hello, what's your name?",
     options: ["PO","L", "HS", "M", "NMM", "SS", "SC"],
-    correctAnswer: 5,
+    correctAnswer: "SS",
+    hint: "...",
+    explanation: "TODO: Explanation here",
+    
   },
   {
     clip: Clips.s3_host + '/clips/error-game/i-buy-coffee-starbucks-incorrect.mp4',
@@ -45,7 +53,7 @@ let aslCards = [
     type: "POA",
     english: "I buy coffee at Starbucks",
     options: ["PO","L", "HS", "M", "NMM", "SS", "SC"],
-    correctAnswer: 2,
+    correctAnswer: "HS",
   },
   {
     clip: Clips.s3_host + '/clips/error-game/he-will-go-ferry-incorrect.mp4',
@@ -53,42 +61,141 @@ let aslCards = [
     type: "POA",
     english: "He will take the ferry",
     options: ["PO","L", "HS", "M", "NMM", "SS", "SC"],
-    correctAnswer: 6,
+    correctAnswer: "SC",
   },
 ]
 
+function unabbreviate(abbr){
+  let abbreviations = {
+    "PO": "Palm Orientation",
+    "L": "Location",
+    "HS": "Handshape",
+    "M": "Movement",
+    "NMM": "Non-manual markers",
+    "SS": "Sentence Structure",
+    "SC": "Sign choice",
+  }
+
+  return abbreviations[abbr]
+}
 
 function POACard(props){
-  let [showCorrect, setShowCorrect] = React.useState(false)
+  return !props.gotItRight ?  <div><POACardDefault {...props} /> </div> :
+          <Fade in={true} timeout={1000}>
+            <div><POACardCorrect {...props} /></div>
+          </Fade>
+}
+
+function POACardDefault(props){
+  let errorOptionToErrorSelection = (x,i)=>{
+    return <Button onClick={()=>{
+      if(x === props.card.correctAnswer){
+        props.setGotItRight(true)
+      }
+    }}>{unabbreviate(x)}</Button>
+  }
 
   return (
-    <Card sx={{ minWidth: 275 }}>
+    <Card>
       <CardContent>
-        {props.card.english}
-        <ReactPlayer
-          url={ props.card.clip }
-          controls={true}
-        />
-        <ReactPlayer
+        <center>
+          <Typography variant="h4" sx={{p: 1}}>
+            {!props.gotItRight ? "What's wrong with this sentence?" : "Correct!" }
+          </Typography>
+        </center>
+        {!props.gotItRight ? 
+          <ReactPlayer
+            url={ props.card.clip }
+            controls={true}
+            width="100%"
+          /> :
+          <>
+            <center>
+              <Typography variant="p" sx={{p: 1}}>
+                This is how you should sign it
+              </Typography>
+            </center>
+            <ReactPlayer
+              url={ props.card.correctClip }
+              controls={true}
+              width="100%"
+            />
+          </>
+        }
+
+        <center>
+        <Typography variant="h6" sx={{p: 1}}>
+          English: {props.card.english}
+        </Typography>
+        {props.gotItRight && <Typography variant="h6" sx={{p: 1}}>
+          ASL Gloss: {props.card.correctClipGloss}
+        </Typography>}
+        </center>
+        {/* <ReactPlayer
           url={ props.card.correctClip }
           controls={true}
-        />
+        /> */}
       </CardContent>
       <CardActions>
-        {props.card.options.map((x,i)=>{
-          return <Button onClick={()=>{
-            if(i === props.card.correctAnswer){
-              props.gotItRight()
-            }
-          }}>{x}</Button>
-        })}
+        <Stack direction="row">
+          <Stack spacing={2}>
+            {props.card.options.slice(0,5).map(errorOptionToErrorSelection)}
+          </Stack>
+          <Stack spacing={2}>
+            {props.card.options.slice(5,7).map(errorOptionToErrorSelection)}
+          </Stack>
+          <div>
+            {props.gotItRight && props.card.explanation}
+          </div>
+        </Stack>
+      </CardActions>
+    </Card>
+  );
+}
+
+function POACardCorrect(props){
+  return (
+    <Card>
+      <CardContent>
+        <center>
+          <Typography variant="h4" sx={{p: 1}}>
+            Correct!
+          </Typography>
+        </center>
+            <center>
+              <Typography variant="p" sx={{p: 1}}>
+                This is how you should sign it
+              </Typography>
+            </center>
+            <ReactPlayer
+              url={ props.card.correctClip }
+              controls={true}
+              width="100%"
+            />
+        <center>
+        <Typography variant="h6" sx={{p: 1}}>
+          English: {props.card.english}
+        </Typography>
+        {props.gotItRight && <Typography variant="h6" sx={{p: 1}}>
+          ASL Gloss: {props.card.correctClipGloss}
+        </Typography>}
+        </center>
+      </CardContent>
+      <CardActions>
+        <Stack direction="row">
+          <div>
+            {props.gotItRight && props.card.explanation}
+          </div>
+        </Stack>
       </CardActions>
     </Card>
   );
 }
 
 function BasicCard(props) {
-  return <POACard {...props} />
+  return <>
+    <POACard {...props} />
+  </>
 }
 
 
@@ -123,34 +230,31 @@ function Intro(){
 function Deck(props){
   // Make a state variable to store the current card's index
   const [cardIndex, setCardIndex] = React.useState(0)
-  const [currentCorrect, setCurrentCorrect] = React.useState(false)
+  const [gotItRight, setGotItRight] = React.useState(false)
 
   React.useEffect(()=>{
     setCardIndex(0)
   },
   [props.startTime])
 
-  const gotItRight = ()=>{
-    setCurrentCorrect(true)
-  }
-
   const prev = ()=>{
     if(cardIndex - 1 >= 0){
       setCardIndex(cardIndex - 1)
-      setCurrentCorrect(false)
+      setGotItRight(false)
     }
   }
 
   const next = ()=>{
     if(cardIndex + 1 < props.cards.length){
       setCardIndex(cardIndex + 1)
-      setCurrentCorrect(false)
+      setGotItRight(false)
     }
   }
 
   return <>
-    <Gamification startTime={props.startTime} currentCorrect={currentCorrect} />
+    {/* <Gamification startTime={props.startTime} currentCorrect={currentCorrect} /> */}
     {props.cards[cardIndex] && <BasicCard card={props.cards[cardIndex]}
+                    setGotItRight={setGotItRight}
                     gotItRight={gotItRight}
                     setCardIndex={setCardIndex}
                     cardIndex={cardIndex}
