08bf34139b7186dfd56daffb0a38d4737e88aeb0
Laura <laheppell@gmail.com>
2024-03-18 19:38:04 -0700

    added text to the navbar buttons

--- asl-trainer/src/NavBar.js
+++ asl-trainer/src/NavBar.js
@@ -3,30 +3,62 @@ import AppBar from '@mui/material/AppBar';
 import Avatar from '@mui/material/Avatar';
 import IconButton from '@mui/material/IconButton';
 import Stack from '@mui/material/Stack';
-import {Link} from "react-router-dom"
+import { Link } from "react-router-dom"
 import LearnIcon from '@mui/icons-material/AutoStories';
 import TimedIcon from '@mui/icons-material/ToggleOff';
 import SettingsIcon from '@mui/icons-material/Settings';
 import ProfileIcon from '@mui/icons-material/AccountCircle';
+import { Typography } from '@mui/material';
 
 export default function NavBar() {
   return (
-      <AppBar position="fixed" color="secondary" sx={{ top: 'auto', bottom: 0 }}>
-          <Stack direction="row" alignItems={"center"} justifyContent="center" spacing={2} >
-            <IconButton>
-              {/* <Link style={{color: "black"}} to={"/"+props.to}>{props.to}</Link> */}
-              <LearnIcon />
-            </IconButton>
-            <IconButton>
-              <TimedIcon />
-            </IconButton>
-            <IconButton>
-              <SettingsIcon />
-            </IconButton>
-            <IconButton>
-              <Avatar alt="Cindy Baker" src="https://mui.com/static/images/avatar/1.jpg" />
-            </IconButton>
+    <AppBar position="fixed" color="secondary" sx={{ top: 'auto', bottom: 0 }}>
+      <Stack direction="row" alignItems={"center"} justifyContent="center" spacing={2} >
+        <IconButton>
+          {/* <Link style={{color: "black"}} to={"/"+props.to}>{props.to}</Link> */}
+          <Stack alignItems={"center"}>
+            <Stack alignItems={"center"} justifyContent="center"  style={{ width: 50, height: 50 }}>
+              <LearnIcon style={{ width: '30px', height: '30px' }} />
+            </Stack>
+            <Typography>
+              learn
+            </Typography>
           </Stack>
-      </AppBar>
+        </IconButton>
+        <IconButton>
+          <Stack alignItems={"center"}>
+            <Stack alignItems={"center"} justifyContent="center"  style={{ width: 50, height: 50 }}>
+              <TimedIcon style={{ width: '50px', height: '50px' }} />
+            </Stack>
+
+            <Typography>timed practice</Typography>
+          </Stack>
+        </IconButton>
+        <IconButton>
+          <Stack alignItems={"center"}>
+            <Stack alignItems={"center"} justifyContent="center"  style={{ width: 50, height: 50 }}>
+              <SettingsIcon style={{ width: '40px', height: '40px' }} />
+            </Stack>
+
+            <Typography>settings</Typography>
+          </Stack>
+        </IconButton>
+        <IconButton>
+
+          <Stack alignItems={"center"}>
+            <Stack alignItems={"center"} justifyContent="center"  style={{ width: 50, height: 50 }}>
+              <Avatar alt="Cindy Baker" src="https://mui.com/static/images/avatar/1.jpg" />
+            </Stack>
+
+            <Typography>profile</Typography>
+          </Stack>
+        </IconButton>
+      </Stack>
+      <Stack direction="row" alignItems={"left"} justifyContent="center" spacing={6} >
+
+      </Stack>
+    </AppBar>
   );
 }
+
+// variant="p" sx={{p: 0}}
