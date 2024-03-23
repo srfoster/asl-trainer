d4d9322678ec5dc6ec7bcd0d8d3182b850bcbc82
Laura <laheppell@gmail.com>
2024-03-23 10:28:23 -0700

    added new game instructions for gloss mistakes

--- asl-trainer/src/feedItems.js
+++ asl-trainer/src/feedItems.js
@@ -5,6 +5,8 @@ let errorInstructions = "<p>The videos below contain mistakes!</p> <p>Mistakes c
 
 let fingerspellingInstructions = "The videos below involve fingerspelling!  Click the word being spelled."
 
+let glossInstructions = "<p>The gloss below does not match the videos! It has mistakes.</p> <p>Find the first gloss word that does not match.</p>"
+
 let instructions = (title, text)=>{
   return {
     text: text, 
@@ -12,7 +14,48 @@ let instructions = (title, text)=>{
     type: "PlainTextFeedItem"
   }
 }
- 
+
+
+// let glossItems = [{
+//   clip: Clips.s3_host + '/clips/error-game/hello-your-name-what-correct.mp4',
+//   clipGloss: "HELLO WHAT YOU NAME?", **note: this will not appear because of structure in App.js. change format to correct answer? 
+//   correctClipGloss: "HELLO YOU NAME WHAT?",
+//   english: "Hello, what's your name?",
+//   //options: ["PO","L", "HS", "M", "NMM", "SS", "SC"],
+//   correctAnswer: {/*type: "SS",*/ word: "WHAT"},
+//   type: "GlossGameFeedItem",
+//   producer: {
+//       username: "christineK",
+//       avatar: "https://mui.com/static/images/avatar/3.jpg"
+//   }
+// },
+// {
+//   clip: Clips.s3_host + '/clips/error-game/i-buy-coffee-starbucks-correct.mp4',
+//   clipGloss: "I COFFEE BUY STARBUCKS", **Note: Should mistakes be sentence structure or can they just be inserted wrong words? 
+//   english: "I buy coffee at Starbucks",
+//   correctClipGloss: "I BUY COFFEE STARBUCKS",
+//   correctAnswer: {word: "I"},
+//   type: "ErrorGameFeedItem",
+//   producer: {
+//       username: "christineK",
+//       avatar: "https://mui.com/static/images/avatar/3.jpg"
+//   }
+
+// },
+// {
+//   clip: Clips.s3_host + '/clips/error-game/he-will-go-ferry-correct.mp4',
+//   clipGloss: "THEY GO CAR" ???
+//   english: "He will take the ferry",
+//   correctClipGloss: "THEY GO FERRY",
+//   correctAnswer: {word: "CAR"},
+//   type: "ErrorGameFeedItem",
+//   producer: {
+//       username: "christineK",
+//       avatar: "https://mui.com/static/images/avatar/3.jpg"
+//   }
+// },
+// ]
+
 let errorGameItems = [{
     clip: Clips.s3_host + '/clips/error-game/hello-your-name-what-incorrect.mp4',
     clipGloss: "HELLO WHAT YOU NAME?",
@@ -56,6 +99,7 @@ let errorGameItems = [{
 ]
 
 export let aslItems = [
+  instructions("Gloss Mistakes", glossInstructions),
   instructions("Find the Error", errorInstructions),
   ... errorGameItems,
   instructions("Fingerspelling", fingerspellingInstructions + " (Coming soon)"),
