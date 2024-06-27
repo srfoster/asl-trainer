26afb0f7f4abb2ff27bf0beeaf4d70825648cf43
Laura <laheppell@gmail.com>
2024-06-26 18:17:22 -0700

    fixing randomize

--- asl-trainer/src/App.js
+++ asl-trainer/src/App.js
@@ -192,7 +192,7 @@ function MultipleChoiceFeedItem(props) {
             id={props.card.id}
             arrangement={props.card.arrangement}
             correctAnswer={props.card.correct_answer}
-            onClick={setWordSelection} gloss={props.card.randomize_options ? shuffle(props.card.answerOptions) : props.card.answerOptions} />
+            onClick={setWordSelection} gloss={props.card.randomize_options ? shuffle(props.card.answer_options) : props.card.answer_options} />
         </Stack>
       </div>
     </FeedCard>
